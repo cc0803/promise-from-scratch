f30a07b5d16d4f245ceeef3399a113408b52a264
cc0803 <simongeldbach@gmail.com>
2024-12-01 17:42:00 +0100

    Still falsy implementation of sqrt

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-function hello() {
-	return "Hello, World!";
+const calculator = {
+	add: (a, b) => a + b,
+	sub: (a, b) => a - b,
+	div: (a, b) => a / b,
+	mul: (a, b) => a * b,
+	sqrt: (a) => {
+		let upper = findBound(a, true);
+		let lower = findBound(a, false);
+		return upper;
+	},
+};
+
+function findBound(n, upper) {
+	let arr = n.toString().split("");
+	let up = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == ".") {
+			break;
+		} else {
+			up.push(arr[i]);
+		}
+	}
+
+	if (upper) {
+		up[up.length - 1] = (Number(up[up.length - 1]) + 1).toString();
+	}
+
+	return Number(up.join(""));
 }
 
-module.exports = hello;
+module.exports = calculator;
--- main.test.js
+++ main.test.js
@@ -1,5 +1,29 @@
-const hello = require("./main");
+const calculator = require("./main");
 
-test("Returns Hello", () => {
-	expect(hello()).toBe("Hello, World!");
+test("Adds numbers", () => {
+	expect(calculator.add(3, -4)).toBe(-1);
+});
+
+test("Subtracts numbers", () => {
+	expect(calculator.sub(3, -4)).toBe(7);
+});
+
+test("Divides", () => {
+	expect(calculator.div(7, 4)).toBe(1.75);
+});
+
+test("Multiplies", () => {
+	expect(calculator.mul(7, 4)).toBe(28);
+});
+
+test("Correct lower bound", () => {
+	expect(calculator.sqrt(4.5)).toBe(4);
+});
+
+test("Correct upper bound", () => {
+	expect(calculator.sqrt(4.5)).toBe(5);
+});
+
+test("Correct range for sqrt", () => {
+	expect(calculator.sqrt(4.5)).toBe([4.1, 4.9]);
 });
